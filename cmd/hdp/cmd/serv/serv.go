2f87f1fc27f090b7b5c617b90bf31f1210d61421
it512 <it512@msn.com>
2025-07-05 09:09:04 +0800

    update

---
+++ cmd/hdp/cmd/serv/serv.go
@@ -0,0 +1,76 @@
+package serv
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/it512/xxl-job-exec"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"gopkg.in/yaml.v3"
+)
+
+var ServCmd = &cobra.Command{
+	Use:   "serv",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		run()
+	},
+}
+var cfgFile string
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	ServCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "config file")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		viper.SetConfigType("yaml")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
+
+func printConf(conf AppConf) {
+	enc := yaml.NewEncoder(os.Stdout)
+	enc.SetIndent(2)
+	enc.Encode(conf)
+	enc.Close()
+}
+
+func run() {
+	var conf AppConf
+	if err := viper.Unmarshal(&conf); err != nil {
+		log.Fatal(err)
+	}
+
+	printConf(conf)
+
+	exec := buildLocalExec(conf)
+	exec.Init()
+
+	app := buildApp(conf)
+	exec.RegJob(app.Name(), app)
+
+	mux := chi.NewMux()
+	mux.Use(middleware.Logger, middleware.Recoverer)
+	mux.Mount("/", xxl.Handle(exec))
+
+	if err := http.ListenAndServe(conf.ServerConf.Addr, mux); err != nil {
+		log.Fatal(err)
+	}
+}
