829413c0f44aed4d0fc16e4665342a1d074bac45
it512 <it512@msn.com>
2025-10-29 17:09:38 +0800

    job

--- xjob/aibee/aibee.py
+++ xjob/aibee/aibee.py
@@ -5,7 +5,16 @@ def traffic_summary():
     pass
 
 
-params = {"key1": "value1", "key2": "value2"}
-r = httpx.get("", params=params)
+params = (
+    ("mall_id", "JFCD_nanjing_zygc"),
+    ("token", "TEtVNkkBaEYkq7shfRO0ABmVHpexJ12Z"),
+    ("entityType", 70),
+    ("startTime", "2025-10-28"),
+    ("endTime", "2025-10-28"),
+    ("interval", "D"),
+)
+r = httpx.get("https://face-event-api.aibee.cn/traffic_summary", params=params)
 
-print(r.url)
+list = r.json()["data"]["list"]
+xx = (x["trafficIn"] for x in list)
+print(sum(xx))
